3d882584ede034a6a7da88e942ba2b379e9eeda8
ferdian3456 <mulyadipermadi21@gmail.com>
2025-05-09 14:57:38 +0700

    add: add additional information

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 type User struct {
-	Username    string `validate:"required|minLen:3"`
+	Username    string `validate:"required|minLen:3" json:"username"` // if you wanted to print in lowercase
 	Email       string `validate:"required|email|maxLen:20"`
 	Gender      string `validate:"in:Male,Female"`
 	Description string `validate:"minLen:10"` // optional field
@@ -26,18 +26,49 @@ func main() {
 		Username:    "a",
 		Email:       "",
 		Gender:      "Mal",
-		Description: "123455",
+		Description: "",
 		Age:         17,
 	}
 
+	testValidator(&user)
+
 	validate.Config(func(opt *validate.GlobalOption) {
 		opt.StopOnError = false
-		opt.SkipOnEmpty = true
+		//opt.SkipOnEmpty = false // default is true
+		fmt.Println(opt.SkipOnEmpty)
+		// SkipOnEmpty vs Omitempty
+		// SkipOnEmpty = global
+		// Omitempty = directly to struct field
 	})
+
+	testValidator(&user)
 
 	// opt.SkipOnEmpty is useful when field is optional
 	// when the field is filled, then it will do the validation like min len
 
+	v := validate.Struct(user)
+
+	errorMap := make(map[string][]string)
+
+	if v.Validate() == false {
+		for field, rules := range v.Errors {
+			for _, msg := range rules {
+				errorMap[field] = append(errorMap[field], msg)
+			}
+		}
+	}
+
+	response := Response{
+		Code:   http.StatusOK,
+		Status: http.StatusText(http.StatusOK),
+		Error:  errorMap,
+	}
+
+	jsonBytes, _ := json.MarshalIndent(response, "", "  ")
+	fmt.Println(string(jsonBytes))
+}
+
+func testValidator(user *User) {
 	v := validate.Struct(user)
 
 	errorMap := make(map[string][]string)
